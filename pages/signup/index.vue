9579b53575da97fcb7c2adfe15a7b9b5a846bebf
Murillo <murillobazilio@gmail.com>
2022-04-11 22:20:26 -0300

    Seletor de tags do cadastro funcionando!

--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -1,114 +1,330 @@
 <template>
-  <div class="d-flex flex-column justify-content-center align-items-center p-3" style="min-height: 100vh; background-color: #F0F0F0;">
+  <div
+    class="d-flex flex-column justify-content-center align-items-center"
+    style="min-height: 100vh; background-color: #f0f0f0"
+  >
     <form>
       <div class="form-group">
         <label for="name" class="form-title">Qual o seu nome?</label>
-        <input type="text" class="form-control" id="name" v-model="name">
+        <input type="text" class="form-control" id="name" v-model="name" />
       </div>
       <div class="form-group">
-        <label for="name" class="form-title">Com o que você trabalha?</label><br>
-        <div class="form-check form-check-inline mb-3">
-          <input class="form-check-input" type="checkbox" id="frontend" value="Front-end" v-model="tags">
+        <label for="name" class="form-title">Com o que você trabalha?</label
+        ><br />
+        <div
+          class="form-check form-check-inline mb-2 position-input"
+          id="position-1"
+          @click="selectPosition('position-1')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="frontend"
+            value="Front-end"
+            v-model="position"
+          />
           <label class="form-check-label" for="frontend">Front-end</label>
         </div>
-        <div class="form-check form-check-inline mb-3">
-          <input class="form-check-input" type="checkbox" id="backend" value="Back-end" v-model="tags">
+        <div
+          class="form-check form-check-inline mb-2 position-input"
+          id="position-2"
+          @click="selectPosition('position-2')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="backend"
+            value="Back-end"
+            v-model="position"
+          />
           <label class="form-check-label" for="backend">Back-end</label>
         </div>
-        <div class="form-check form-check-inline mb-3">
-          <input class="form-check-input" type="checkbox" id="fullstack" value="Full Stack" v-model="tags">
+        <div
+          class="form-check form-check-inline mb-2 position-input"
+          id="position-3"
+          @click="selectPosition('position-3')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="fullstack"
+            value="Full Stack"
+            v-model="position"
+          />
           <label class="form-check-label" for="fullstack">Full Stack</label>
         </div>
-        <div class="form-check form-check-inline mb-3">
-          <input class="form-check-input" type="checkbox" id="ux" value="UX Designer" v-model="tags">
+        <div
+          class="form-check form-check-inline mb-2 position-input"
+          id="position-4"
+          @click="selectPosition('position-4')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="ux"
+            value="UX Design"
+            v-model="position"
+          />
           <label class="form-check-label" for="ux">UX Design</label>
         </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" id="ui" value="UI Designer" v-model="tags">
+        <div
+          class="form-check form-check-inline mb-2 position-input"
+          id="position-5"
+          @click="selectPosition('position-5')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="ui"
+            value="UI Design"
+            v-model="position"
+          />
           <label class="form-check-label" for="ui">UI Design</label>
+        </div>
+        <div
+          class="form-check form-check-inline mb-2 position-input"
+          id="position-6"
+          @click="selectPosition('position-6')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="ux/ui"
+            value="UX/UI Design"
+            v-model="position"
+          />
+          <label class="form-check-label" for="ux/ui">UX/UI Design</label>
         </div>
       </div>
       <div class="form-group">
-        <label for="name" class="form-title">Qual o seu nível de senioridade?</label><br>
-        <div class="form-check form-check-inline mb-3">
-          <input class="form-check-input" type="radio" id="trainee" value="Trainee" v-model="level">
+        <label for="name" class="form-title"
+          >Qual o seu nível de senioridade?</label
+        ><br />
+        <div
+          class="form-check form-check-inline mb-2 level-input"
+          id="level-1"
+          @click="selectLevel('level-1')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="trainee"
+            value="Trainee"
+            v-model="level"
+          />
           <label class="form-check-label" for="trainee">Trainee</label>
         </div>
-        <div class="form-check form-check-inline mb-3">
-          <input class="form-check-input" type="radio" id="junior" value="Júnior" v-model="level">
+        <div
+          class="form-check form-check-inline mb-2 level-input"
+          id="level-2"
+          @click="selectLevel('level-2')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="junior"
+            value="Júnior"
+            v-model="level"
+          />
           <label class="form-check-label" for="junior">Júnior</label>
         </div>
-        <div class="form-check form-check-inline mb-3">
-          <input class="form-check-input" type="radio" id="pleno" value="Pleno" v-model="level">
+        <div
+          class="form-check form-check-inline mb-2 level-input"
+          id="level-3"
+          @click="selectLevel('level-3')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="pleno"
+            value="Pleno"
+            v-model="level"
+          />
           <label class="form-check-label" for="pleno">Pleno</label>
         </div>
-        <div class="form-check form-check-inline mb-3">
-          <input class="form-check-input" type="radio" id="senior" value="Senior" v-model="level">
-          <label class="form-check-label" for="senior">Senior</label>
+        <div
+          class="form-check form-check-inline mb-2 level-input"
+          id="level-4"
+          @click="selectLevel('level-4')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="senior"
+            value="Sênior"
+            v-model="level"
+          />
+          <label class="form-check-label" for="senior">Sênior</label>
         </div>
-      </div>  
-        <div class="form-group">
-          <label for="tech" class="form-title">Com qual tecnologia você trabalha?</label>
-          <input type="text" class="form-control" id="tech" v-model="tech">
+      </div>
+      <div class="form-group">
+        <label for="name" class="form-title">Você se dispõe a ser mentor?</label
+        ><br />
+        <div
+          class="form-check form-check-inline mb-2 mentor-input"
+          id="mentor-1"
+          @click="selectMentor('mentor-1')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="yes"
+            :value="true"
+            v-model="isMentor"
+          />
+          <label class="form-check-label" for="yes">Sim</label>
         </div>
-        <div class="form-group">
-          <label class="form-title">Links de comunicação</label>
-          <input type="text" class="form-control mb-3" id="linkedin" v-model="links.linkedin" placeholder="Linkedin">
-          <input type="text" class="form-control mb-3" id="teams" v-model="links.teams" placeholder="Teams">
-          <input type="text" class="form-control mb-3" id="whatsapp" v-model="links.whatsapp" placeholder="Whatsapp">
+        <div
+          class="form-check form-check-inline mb-2 mentor-input"
+          id="mentor-2"
+          @click="selectMentor('mentor-2')"
+        >
+          <input
+            class="form-check-input"
+            type="radio"
+            id="no"
+            :value="false"
+            v-model="isMentor"
+          />
+          <label class="form-check-label" for="no">Não</label>
         </div>
-      <p>Áreas selecionadas: {{ tags }}</p>
+      </div>
+      <div class="form-group">
+        <label for="tech" class="form-title mb-0"
+          >Quais ferramentas você domina?</label
+        >
+        <input
+          type="text"
+          class="form-control"
+          id="tech"
+          v-model="techs"
+          placeholder="Utilize apenas vírgulas (sem espaços) para separar as ferramentas."
+        />
+      </div>
+      <div class="form-group">
+        <label class="form-title">Links para você se conectar</label>
+        <div class="d-flex align-items-center mb-2" style="gap: 1ch">
+          <font-awesome-icon icon="fab fa-linkedin" style="font-size: 32px" />
+          <input
+            type="text"
+            class="form-control"
+            id="linkedin"
+            v-model="links.linkedin"
+            placeholder="Linkedin"
+          />
+        </div>
+        <div class="d-flex align-items-center mb-2" style="gap: 1ch">
+          <font-awesome-icon icon="fab fa-microsoft" style="font-size: 32px" />
+          <input
+            type="text"
+            class="form-control"
+            id="teams"
+            v-model="links.teams"
+            placeholder="Teams"
+          />
+        </div>
+        <div class="d-flex align-items-center mb-2" style="gap: 1ch">
+          <font-awesome-icon icon="fab fa-whatsapp" style="font-size: 32px" />
+          <input
+            type="text"
+            class="form-control"
+            id="whatsapp"
+            v-model="links.whatsapp"
+            placeholder="Whatsapp"
+          />
+        </div>
+      </div>
       <!-- Inserir type=submit aqui para enviar o form caso necessário -->
     </form>
-    <button @click="submitTest" class="btn btn-primary">Confirmar</button>  
+    <button @click="submitTest" class="form-btn">Salvar</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Cadastro',
+  name: "Cadastro",
   data() {
     return {
       name: "",
       email: this.$route.query.email,
-      password: "",
-      passwordConfirmation: "",
-      tags: [],
-      tech: "",
+      isMentor: false,
+      position: "",
+      level: "",
+      techs: [""],
       links: {
         linkedin: "",
         teams: "",
-        whatsapp: ""
+        whatsapp: "",
       },
-      isClicked: false,
-    }
+      picture: "",
+    };
   },
   methods: {
     submitTest() {
-      console.log(this.name)
-      console.log(this.email)
-      console.log(this.tags)
-      console.log(this.tech)
-      console.log(this.links)
-    }
+      //TODO -> this.techs tem que dar Array.split(',').
+      console.log(this.name);
+      console.log(this.position);
+      console.log(this.level);
+      console.log(this.mentor);
+      console.log(this.email);
+      // console.log(this.techs.split(","));
+      console.log(this.links);
+    },
+    selectPosition(id) {
+      let allPositions = document.querySelectorAll(".position-input");
+      allPositions.forEach((element) => {
+        element.classList.remove("selected");
+      });
+      let selectedElement = document.getElementById(id);
+      selectedElement.classList.add("selected");
+    },
+    selectLevel(id) {
+      let allLevels = document.querySelectorAll(".level-input");
+      allLevels.forEach((element) => {
+        element.classList.remove("selected");
+      });
+      let selectedElement = document.getElementById(id);
+      selectedElement.classList.add("selected");
+    },
+    selectMentor(id) {
+      let allMentors = document.querySelectorAll(".mentor-input");
+      allMentors.forEach((element) => {
+        element.classList.remove("selected");
+      });
+      let selectedElement = document.getElementById(id);
+      selectedElement.classList.add("selected");
+    },
   },
   computed: {
     userEmail() {
-      return this.$route.params.email
-    }
-  }
-}
+      return this.$route.params.email;
+    },
+  },
+};
 </script>
 
 <style scoped>
 * {
-  font-family: 'Manrope';
+  font-family: "Manrope";
+}
+
+form {
+  padding: 20px;
 }
 
 .form-check {
-  background-color: #aaaaaa;
-  border-radius: 15px;
+  background-color: #ffffff;
+  border: solid 2px #d3d3d3;
+  border-radius: 20px;
   cursor: pointer;
+  box-shadow: 0px 4px 4px 0px #00000040;
+  font-weight: bold;
+  min-width: 100px;
+  justify-content: center;
+}
+
+.form-control::placeholder {
+  font-size: 12px;
 }
 
 .form-check input {
@@ -125,14 +341,30 @@ export default {
 }
 
 .form-check:hover {
-  background-color: #ff932e;
+  background-color: #5251a2;
+  color: #ffffff;
 }
 
-.clicked {
-  background-color: #ff932e;
+.selected {
+  background-color: #5251a2;
+  color: #ffffff;
+}
+
+.form-group input {
+  border-radius: 20px;
 }
 
 .form-title {
-  font-size: 1.3rem;
+  font-size: 1.5rem;
+}
+
+.form-btn {
+  background: #5251a2;
+  border-radius: 20px;
+  padding: 10px 30px;
+  border: 0;
+  font-size: 14px;
+  font-weight: bold;
+  color: #ffffff;
 }
 </style>
--- utils/mocks.js
+++ utils/mocks.js
@@ -2,86 +2,32 @@ export const users =  [
   {
     id: 1,
     name: "Murillo",
-    mentor: false,
-    tags: ["Frontend", "Javascript"],
-    position: "Fullstack developer",
     email: "",
-    tech: "",
+    isMentor: false,
+    position: "Front-end",
+    level: "Trainee",
+    techs: ["Vue.js", "Javascript"],
     links: {
-    linkedin: "",
-    teams: "",
-    whatsapp: ""
-    }
+      linkedin: "",
+      teams: "",
+      whatsapp: ""
+    },
+    picture: ""
   },
   {
     id: 2,
     name: "Celso",
-    mentor: true,
-    tags: ["Backend", "PHP"],
-    position: "Fullstack developer",
     email: "",
-    tech: "",
+    isMentor: false,
+    position: "Front-end",
+    level: "Trainee",
+    techs: ["Vue.js", "Javascript"],
     links: {
       linkedin: "",
       teams: "",
       whatsapp: ""
-    }
-  },
-  {
-    id: 3,
-    name: "Decio",
-    mentor: true,
-    tags: ["Fullstack", "Javascript", "PHP"],
-    position: "Fullstack developer",
-    email: "",
-    tech: "",
-    links: {
-      linkedin: "",
-      teams: "",
-      whatsapp: ""
-    }
-  },
-  {
-    id: 4,
-    name: "Lucas",
-    mentor: true,
-    tags: ["UX", "UI"],
-    position: "UX Researcher",
-    email: "",
-    tech: "",
-    links: {
-      linkedin: "",
-      teams: "",
-      whatsapp: ""
-    }
-  },
-  {
-    id: 5,
-    name: "Markellen",
-    mentor: true,
-    tags: ["UX", "UI"],
-    position: "UX/UI Designer",
-    email: "",
-    tech: "",
-    links: {
-      linkedin: "",
-      teams: "",
-      whatsapp: ""
-    }
-  },
-  {
-    id: 6,
-    name: "Andrew",
-    mentor: true,
-    tags: ["UX", "UI"],
-    position: "UI Designer",
-    email: "",
-    tech: "",
-    links: {
-      linkedin: "",
-      teams: "",
-      whatsapp: ""
-    }
+    },
+    picture: ""
   },
 ];
 
